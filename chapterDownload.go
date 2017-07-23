3f549544d1275350631da433369428f1d1b82e50
freddieptf <freddmuiru@gmail.com>
2017-07-23 11:24:30 +0300

    make it go gettable

---
+++ chapterDownload.go
@@ -0,0 +1,8 @@
+package main
+
+type chapterDownload struct {
+	url     string
+	manga   string //name of the manga
+	chapter string
+	volume  string //what volume the chapter belongs to, optional really
+}
