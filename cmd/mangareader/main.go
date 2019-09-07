e087f2c6b286ce523c3cdf8505dcac77bed63ec2
beans <229907-freddieptf@users.noreply.gitlab.com>
2019-09-07 15:27:53 +0000

    use flagset

--- cmd/mangareader/main.go
+++ cmd/mangareader/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"flag"
 	"fmt"
 	cli "github.com/freddieptf/manga-scraper/pkg/cli"
@@ -9,19 +10,35 @@ import (
 )
 
 var (
-	archive            = flag.Bool("cbz", true, "save to cbz file format")
-	maxActiveDownloads = flag.Int("n", 1, "max number of concurrent downloads")
-	manga              = flag.String("manga", "", "the name of the manga")
+	archive            bool
+	maxActiveDownloads int 
+	manga string
 )
 
 func main() {
-	flag.Parse()
-	args := flag.Args()
-	if *manga == "" {
-		fmt.Println("no manga name passed")
-		flag.PrintDefaults()
+	cmdFlagSet := flag.NewFlagSet("get", flag.ExitOnError)
+	cmdFlagSet.StringVar(&manga, "manga", "", "manga name")
+	cmdFlagSet.IntVar(&maxActiveDownloads, "n", 1, "max number of parallel downloads")
+	cmdFlagSet.BoolVar(&archive, "cbz", true, "save as cbz file format")
+
+	osArgs := os.Args
+	if len(osArgs) <= 1 {
+		cmdFlagSet.Usage()
 		os.Exit(1)
 	}
-	source := &mangareader.ReaderManga{}
-	cli.Get(*maxActiveDownloads, *manga, cli.GetChapterRangeFromArgs(&args), archive, source)
+
+	switch osArgs[1] {
+	case "get":
+		if err := cmdFlagSet.Parse(os.Args[2:]); err != nil {
+			log.Fatal(err)
+		}
+		if manga == "" {
+			fmt.Println("no manga name was passed")
+			cmdFlagSet.Usage()
+			os.Exit(1)
+		}
+		args := cmdFlagSet.Args()
+		source := &mangareader.ReaderManga{}
+		cli.Get(maxActiveDownloads, manga, cli.GetChapterRangeFromArgs(&args), &archive, source)
+	}
 }
